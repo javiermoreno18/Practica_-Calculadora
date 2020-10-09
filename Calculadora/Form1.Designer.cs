4c86f243d8975edc8ef874a775337cd68ea8b6d7
javiermoreno18 <72530066+javiermoreno18@users.noreply.github.com>
2020-10-09 11:45:32 -0700

    Add files via upload

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boton1 = new System.Windows.Forms.Button();
+            this.boton2 = new System.Windows.Forms.Button();
+            this.boton3 = new System.Windows.Forms.Button();
+            this.boton4 = new System.Windows.Forms.Button();
+            this.boton5 = new System.Windows.Forms.Button();
+            this.boton6 = new System.Windows.Forms.Button();
+            this.boton7 = new System.Windows.Forms.Button();
+            this.boton8 = new System.Windows.Forms.Button();
+            this.boton9 = new System.Windows.Forms.Button();
+            this.botonSuma = new System.Windows.Forms.Button();
+            this.botonResta = new System.Windows.Forms.Button();
+            this.botonMult = new System.Windows.Forms.Button();
+            this.botonDiv = new System.Windows.Forms.Button();
+            this.boton0 = new System.Windows.Forms.Button();
+            this.botonClear = new System.Windows.Forms.Button();
+            this.botonResultado = new System.Windows.Forms.Button();
+            this.textBoxPantalla = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // boton1
+            // 
+            this.boton1.Location = new System.Drawing.Point(73, 148);
+            this.boton1.Name = "boton1";
+            this.boton1.Size = new System.Drawing.Size(80, 30);
+            this.boton1.TabIndex = 0;
+            this.boton1.Text = "1";
+            this.boton1.UseVisualStyleBackColor = true;
+            this.boton1.Click += new System.EventHandler(this.boton1_Click);
+            // 
+            // boton2
+            // 
+            this.boton2.Location = new System.Drawing.Point(159, 148);
+            this.boton2.Name = "boton2";
+            this.boton2.Size = new System.Drawing.Size(80, 30);
+            this.boton2.TabIndex = 1;
+            this.boton2.Text = "2";
+            this.boton2.UseVisualStyleBackColor = true;
+            this.boton2.Click += new System.EventHandler(this.boton2_Click);
+            // 
+            // boton3
+            // 
+            this.boton3.Location = new System.Drawing.Point(245, 148);
+            this.boton3.Name = "boton3";
+            this.boton3.Size = new System.Drawing.Size(80, 30);
+            this.boton3.TabIndex = 2;
+            this.boton3.Text = "3";
+            this.boton3.UseVisualStyleBackColor = true;
+            this.boton3.Click += new System.EventHandler(this.boton3_Click);
+            // 
+            // boton4
+            // 
+            this.boton4.Location = new System.Drawing.Point(73, 112);
+            this.boton4.Name = "boton4";
+            this.boton4.Size = new System.Drawing.Size(80, 30);
+            this.boton4.TabIndex = 3;
+            this.boton4.Text = "4";
+            this.boton4.UseVisualStyleBackColor = true;
+            this.boton4.Click += new System.EventHandler(this.boton4_Click);
+            // 
+            // boton5
+            // 
+            this.boton5.Location = new System.Drawing.Point(159, 112);
+            this.boton5.Name = "boton5";
+            this.boton5.Size = new System.Drawing.Size(80, 30);
+            this.boton5.TabIndex = 4;
+            this.boton5.Text = "5";
+            this.boton5.UseVisualStyleBackColor = true;
+            this.boton5.Click += new System.EventHandler(this.boton5_Click);
+            // 
+            // boton6
+            // 
+            this.boton6.Location = new System.Drawing.Point(245, 112);
+            this.boton6.Name = "boton6";
+            this.boton6.Size = new System.Drawing.Size(80, 30);
+            this.boton6.TabIndex = 5;
+            this.boton6.Text = "6";
+            this.boton6.UseVisualStyleBackColor = true;
+            this.boton6.Click += new System.EventHandler(this.boton6_Click);
+            // 
+            // boton7
+            // 
+            this.boton7.Location = new System.Drawing.Point(73, 76);
+            this.boton7.Name = "boton7";
+            this.boton7.Size = new System.Drawing.Size(80, 30);
+            this.boton7.TabIndex = 6;
+            this.boton7.Text = "7";
+            this.boton7.UseVisualStyleBackColor = true;
+            this.boton7.Click += new System.EventHandler(this.boton7_Click);
+            // 
+            // boton8
+            // 
+            this.boton8.Location = new System.Drawing.Point(159, 76);
+            this.boton8.Name = "boton8";
+            this.boton8.Size = new System.Drawing.Size(80, 30);
+            this.boton8.TabIndex = 7;
+            this.boton8.Text = "8";
+            this.boton8.UseVisualStyleBackColor = true;
+            this.boton8.Click += new System.EventHandler(this.boton8_Click);
+            // 
+            // boton9
+            // 
+            this.boton9.Location = new System.Drawing.Point(245, 76);
+            this.boton9.Name = "boton9";
+            this.boton9.Size = new System.Drawing.Size(80, 30);
+            this.boton9.TabIndex = 8;
+            this.boton9.Text = "9";
+            this.boton9.UseVisualStyleBackColor = true;
+            this.boton9.Click += new System.EventHandler(this.boton9_Click);
+            // 
+            // botonSuma
+            // 
+            this.botonSuma.Location = new System.Drawing.Point(331, 76);
+            this.botonSuma.Name = "botonSuma";
+            this.botonSuma.Size = new System.Drawing.Size(80, 30);
+            this.botonSuma.TabIndex = 9;
+            this.botonSuma.Text = "+";
+            this.botonSuma.UseVisualStyleBackColor = true;
+            this.botonSuma.Click += new System.EventHandler(this.botonSuma_Click);
+            // 
+            // botonResta
+            // 
+            this.botonResta.Location = new System.Drawing.Point(331, 112);
+            this.botonResta.Name = "botonResta";
+            this.botonResta.Size = new System.Drawing.Size(80, 30);
+            this.botonResta.TabIndex = 10;
+            this.botonResta.Text = "-";
+            this.botonResta.UseVisualStyleBackColor = true;
+            this.botonResta.Click += new System.EventHandler(this.botonResta_Click);
+            // 
+            // botonMult
+            // 
+            this.botonMult.Location = new System.Drawing.Point(331, 148);
+            this.botonMult.Name = "botonMult";
+            this.botonMult.Size = new System.Drawing.Size(80, 30);
+            this.botonMult.TabIndex = 11;
+            this.botonMult.Text = "x";
+            this.botonMult.UseVisualStyleBackColor = true;
+            this.botonMult.Click += new System.EventHandler(this.botonMult_Click);
+            // 
+            // botonDiv
+            // 
+            this.botonDiv.Location = new System.Drawing.Point(331, 184);
+            this.botonDiv.Name = "botonDiv";
+            this.botonDiv.Size = new System.Drawing.Size(80, 30);
+            this.botonDiv.TabIndex = 12;
+            this.botonDiv.Text = "/";
+            this.botonDiv.UseVisualStyleBackColor = true;
+            this.botonDiv.Click += new System.EventHandler(this.botonDiv_Click);
+            // 
+            // boton0
+            // 
+            this.boton0.Location = new System.Drawing.Point(159, 184);
+            this.boton0.Name = "boton0";
+            this.boton0.Size = new System.Drawing.Size(80, 30);
+            this.boton0.TabIndex = 13;
+            this.boton0.Text = "0";
+            this.boton0.UseVisualStyleBackColor = true;
+            this.boton0.Click += new System.EventHandler(this.boton0_Click);
+            // 
+            // botonClear
+            // 
+            this.botonClear.Location = new System.Drawing.Point(73, 184);
+            this.botonClear.Name = "botonClear";
+            this.botonClear.Size = new System.Drawing.Size(80, 30);
+            this.botonClear.TabIndex = 14;
+            this.botonClear.Text = "CLEAR";
+            this.botonClear.UseVisualStyleBackColor = true;
+            this.botonClear.Click += new System.EventHandler(this.botonClear_Click);
+            // 
+            // botonResultado
+            // 
+            this.botonResultado.Location = new System.Drawing.Point(245, 184);
+            this.botonResultado.Name = "botonResultado";
+            this.botonResultado.Size = new System.Drawing.Size(80, 30);
+            this.botonResultado.TabIndex = 15;
+            this.botonResultado.Text = "=";
+            this.botonResultado.UseVisualStyleBackColor = true;
+            this.botonResultado.Click += new System.EventHandler(this.botonResultado_Click);
+            // 
+            // textBoxPantalla
+            // 
+            this.textBoxPantalla.Location = new System.Drawing.Point(73, 30);
+            this.textBoxPantalla.Multiline = true;
+            this.textBoxPantalla.Name = "textBoxPantalla";
+            this.textBoxPantalla.Size = new System.Drawing.Size(338, 40);
+            this.textBoxPantalla.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.textBoxPantalla);
+            this.Controls.Add(this.botonResultado);
+            this.Controls.Add(this.botonClear);
+            this.Controls.Add(this.boton0);
+            this.Controls.Add(this.botonDiv);
+            this.Controls.Add(this.botonMult);
+            this.Controls.Add(this.botonResta);
+            this.Controls.Add(this.botonSuma);
+            this.Controls.Add(this.boton9);
+            this.Controls.Add(this.boton8);
+            this.Controls.Add(this.boton7);
+            this.Controls.Add(this.boton6);
+            this.Controls.Add(this.boton5);
+            this.Controls.Add(this.boton4);
+            this.Controls.Add(this.boton3);
+            this.Controls.Add(this.boton2);
+            this.Controls.Add(this.boton1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora Francisco Javier Moreno Martinez";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button boton1;
+        private System.Windows.Forms.Button boton2;
+        private System.Windows.Forms.Button boton3;
+        private System.Windows.Forms.Button boton4;
+        private System.Windows.Forms.Button boton5;
+        private System.Windows.Forms.Button boton6;
+        private System.Windows.Forms.Button boton7;
+        private System.Windows.Forms.Button boton8;
+        private System.Windows.Forms.Button boton9;
+        private System.Windows.Forms.Button botonSuma;
+        private System.Windows.Forms.Button botonResta;
+        private System.Windows.Forms.Button botonMult;
+        private System.Windows.Forms.Button botonDiv;
+        private System.Windows.Forms.Button boton0;
+        private System.Windows.Forms.Button botonClear;
+        private System.Windows.Forms.Button botonResultado;
+        private System.Windows.Forms.TextBox textBoxPantalla;
+    }
+}
+
